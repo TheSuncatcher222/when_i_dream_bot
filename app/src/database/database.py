90de2593abb5030f0809bb68f646634a70f9bd8b
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-22 20:55:55 +0300

    Add async_engine, async_session_maker, sync_engine, get_async_session, Base, TableNames, RedisKeys, redis_engine

---
+++ app/src/database/database.py
@@ -0,0 +1,68 @@
+"""
+Модуль соединения с базой данных через SQLAlchemy.
+"""
+
+from typing import AsyncGenerator
+
+from redis import Redis
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.src.config.config import settings
+
+DATABASE_ASYNC_URL: str = f'postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.POSTGRES_DB}'
+DATABASE_SYNC_URL: str = f'postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.POSTGRES_DB}'
+
+async_engine: AsyncEngine = create_async_engine(
+    url=DATABASE_ASYNC_URL,
+    echo=settings.DEBUG_DB,
+)
+
+async_session_maker: async_sessionmaker = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False,
+)
+
+sync_engine = create_engine(
+    DATABASE_SYNC_URL,
+    echo=settings.DEBUG_DB,
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+class Base(DeclarativeBase):
+    """Инициализирует фабрику создания декларативных классов моделей."""
+
+
+class TableNames:
+    """
+    Класс представления названий таблиц в базе данных.
+    """
+
+    game: str = 'table_game'
+    image: str = 'table_image'
+    user: str = 'table_user'
+
+
+class RedisKeys:
+    """Класс представления Redis ключей."""
+
+    __PREFIX_SRC: str = 'src_cache_'
+
+
+redis_engine: Redis = Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB_CACHE,
+    decode_responses=True,
+)
