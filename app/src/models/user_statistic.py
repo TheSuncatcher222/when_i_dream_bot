cf1be605e7c4619b266ea04ed0d6154c96d3e8d1
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-22 22:28:30 +0300

    Add UserStatistic

---
+++ app/src/models/user_statistic.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import (
+    DateTime,
+    ForeignKey,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+from sqlalchemy.sql import expression
+
+from app.src.database.database import (
+    Base,
+    TableNames,
+)
+
+if TYPE_CHECKING:
+    from app.src.models.user import User
+
+
+class UserStatistic(Base):
+    """Декларативная модель представления статистики пользователя."""
+
+    __tablename__ = TableNames.user_statistic
+    __table_args__ = {'comment': 'Статистика пользователей'}
+
+    # Primary keys.
+
+    id: Mapped[int] = mapped_column(
+        comment='ID',
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    # Fields.
+
+    total_score: Mapped[int] = mapped_column(
+        comment='Общее количество очков',
+        server_default=0,
+    )
+    total_score_buka: Mapped[int] = mapped_column(
+        comment='Количество очков за буку',
+        server_default=0,
+    )
+    total_score_fairy: Mapped[int] = mapped_column(
+        comment='Количество очков за фею',
+        server_default=0,
+    )
+    total_score_sandman: Mapped[int] = mapped_column(
+        comment='Количество очков за песочного человека',
+        server_default=0,
+    )
+    total_wins: Mapped[int] = mapped_column(
+        comment='Общее количество побед',
+        server_default=0,
+    )
+    total_quits: Mapped[int] = mapped_column(
+        comment='Общее количество выходов',
+        server_default=0,
+    )
+    last_game_date: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        comment='Последняя игра',
+        nullable=True,
+        server_default=expression.null(),
+    )
+
+    # Foreign keys.
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            column=f'{TableNames.user}.id',
+            name=f'{TableNames.user_statistic}_{TableNames.user}_fkey',
+            ondelete='CASCADE',
+        ),
+        comment='ID пользователя',
+        nullable=False,
+        unique=True,
+    )
+
+    # Relationships.
+
+    user: Mapped['User'] = relationship(
+        'User',
+        back_populates='statistics',
+    )
