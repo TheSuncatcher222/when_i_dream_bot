b30b46fb9b80ea04e4f85b24f689815d42e4a552
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-22 22:26:34 +0300

    Add UserParams

---
+++ app/src/validators/user.py
@@ -0,0 +1,17 @@
+"""
+Модуль с валидаторами моделей базы данных приложения "user".
+"""
+
+
+class UserParams:
+    """
+    Параметры пользователей.
+    """
+
+    # INFO. Согласно Alpha-2 ISO 3166-1
+    COUNTRY_LEN_MAX: int = 2
+    # INFO. Идентификатор меньше, чем 52 бита (4.5e+15) + символ '-'
+    ID_TELEGRAM_LEN_MAX: int = 16 + 1
+    NAME_FIRST_LEN_MAX: int = 64
+    NAME_LAST_LEN_MAX: int = 64
+    USERNAME_LEN_MAX: int = 32
