a1ceed7ee1d357a245c49b43ae8a629395949f1f
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-23 12:21:38 +0300

    Register Image, User, UserAchievement, UserStatistic

---
+++ app/src/models/__init__.py
@@ -0,0 +1,11 @@
+from app.src.models.image import Image
+from app.src.models.user import User
+from app.src.models.user_achievement import UserAchievement
+from app.src.models.user_statistic import UserStatistic
+
+__all__ = (
+    'Image',
+    'User',
+    'UserAchievement',
+    'UserStatistic',
+)
