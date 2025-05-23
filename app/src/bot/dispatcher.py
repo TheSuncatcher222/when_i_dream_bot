c2b2620697502272bbed0109d57c1a28d9fc18d5
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-23 12:14:28 +0300

    Refactor, register help, ping

--- app/src/bot/dispatcher.py
+++ app/src/bot/dispatcher.py
@@ -5,20 +5,18 @@ from aiogram import (
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from app.src.bot.routers.fallback import router as fallback
+from app.src.bot.routers.help import router as help
+from app.src.bot.routers.ping import router as ping
 from app.src.bot.routers.start import router as start
 
 dp: Dispatcher = Dispatcher(
     storage=MemoryStorage(),
 )
 
-routers: tuple[Router] = ()
-
-# WARNING! Нельзя редактировать:
 routers: tuple[Router] = (
-    start,
-    *routers,
-    fallback,
+    help,
+    ping,
 )
 
-for router in routers:
+for router in (start, *routers, fallback):
     dp.include_router(router)
