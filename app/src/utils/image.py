b8bb743e3f118c898933859cc36d99596ea5d75c
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-27 15:18:38 +0300

    Update sync_images, add word rotation, refactor

--- app/src/utils/image.py
+++ app/src/utils/image.py
@@ -16,6 +16,8 @@ from aiogram.types import (
     FSInputFile,
     Message,
 )
+from PIL import Image
+from tempfile import NamedTemporaryFile
 
 from app.src.bot.bot import bot
 from app.src.config.config import (
@@ -42,6 +44,7 @@ async def sync_images() -> None:
             image.local_path: {
                 'id': image.id,
                 'id_telegram': image.id_telegram,
+                'id_telegram_rotated': image.id_telegram_rotated,
             }
             for image
             in await image_crud.retrieve_all(session=session, limit=None)
@@ -49,7 +52,7 @@ async def sync_images() -> None:
 
     semaphore: AsyncSemaphore = AsyncSemaphore(value=5)
     tasks: list[AsyncTask] = [
-        __inspect_obj_with_semaphore(
+        __sync_image_with_semaphore(
             obj=obj,
             dir_name=dir.name,
             db_images=db_images,
@@ -78,78 +81,129 @@ async def sync_images() -> None:
     )
 
 
-async def __inspect_obj(
+async def __check_if_is_needed_to_sync(
     obj: Path,
-    dir_name: str,
-    db_images: dict[str, str | int],
-) -> None:
-    """Проверяет один объект в рамках логики функции check_images."""
+    db_obj: dict[str, str | int],
+) -> bool:
     if obj.is_dir() or not obj.name.endswith('.jpg'):
         await bot.send_message(
             chat_id=settings.ADMIN_NOTIFY_ID,
             text=f'Find unexpected object {obj}, it is not ".jpg" image',
         )
-        return
+        return False
 
-    obj_local_path: str = str(obj.relative_to(Dirs.DIR_RES))
-    db_obj: dict[str, str] = db_images.pop(obj_local_path, None)
     if db_obj and await bot.get_file(file_id=db_obj['id_telegram']):
+        if not obj.is_relative_to(Dirs.DIR_WORDS) or await bot.get_file(file_id=db_obj['id_telegram_rotated']):
+            return False
+
+    return True
+
+
+async def __sync_image(
+    obj: Path,
+    dir_name: str,
+    db_images: dict[str, str | int],
+) -> None:
+    """Проверяет один объект в рамках логики функции check_images."""
+    obj_local_path: str = str(obj.relative_to(Dirs.DIR_RES))
+    db_obj: dict[str, str | int] | None = db_images.pop(obj_local_path, None)
+
+    if not await __check_if_is_needed_to_sync(obj=obj, db_obj=db_obj):
         return
 
-    while 1:
-        try:
-            message: Message = await bot.send_photo(
-                chat_id=settings.ADMIN_NOTIFY_ID,
-                photo=FSInputFile(path=obj),
-                caption='Perform image res sync...',
-                disable_notification=True,
-            )
-            break
-        except TelegramRetryAfter:
-            await asyncio_sleep(15)
-
+    obj_data: dict[str, any] = await __upload_images_to_telegram(
+        obj=obj,
+        obj_local_path=obj_local_path,
+        db_obj=db_obj,
+        dir_name=dir_name,
+    )
     async with async_session_maker() as session:
         if db_obj:
             await image_crud.update_by_id(
                 obj_id=db_obj['id'],
-                obj_dat={'id_telegram': message.photo[-1].file_id},
+                obj_dat=obj_data,
                 session=session,
             )
         else:
-            order, card_name = __parse_obj_name(obj=obj, dir_name=dir_name)
             await image_crud.create(
-                obj_data={
-                    'id_telegram': message.photo[-1].file_id,
-                    'local_path': obj_local_path,
-                    'name': card_name,
-                    'order': order,
-                    'category': ImageCategory.get_category_by_dir(dir_name=dir_name),
-                },
+                obj_data=obj_data,
                 session=session,
             )
 
-    await delete_messages_list(
-        chat_id=message.chat.id,
-        messages_ids=[message.message_id],
-    )
+
+def __parse_obj_name(obj: Path, dir_name: str) -> str:
+    """Парсит название изображения."""
+    if dir_name == Dirs.WORDS:
+        _, card_name_first, card_name_second = obj.stem.split('_')
+        card_name: str = f'{card_name_first} | {card_name_second}'
+    else:
+        _, card_name = obj.stem.split('_')
+    card_name: str = re_sub(pattern='-', repl=' ', string=card_name)
+    return card_name
 
 
-async def __inspect_obj_with_semaphore(
+async def __sync_image_with_semaphore(
     obj: Path,
     dir_name: str,
     db_images: dict[str, str | int],
     semaphore: AsyncSemaphore,
 ) -> None:
     async with semaphore:
-        await __inspect_obj(obj=obj, dir_name=dir_name, db_images=db_images)
+        await __sync_image(obj=obj, dir_name=dir_name, db_images=db_images)
 
 
-def __parse_obj_name(obj: Path, dir_name: str) -> tuple[int, str]:
-    """Парсит название изображения."""
-    if dir_name == Dirs.WORDS:
-        order, card_name_first, card_name_second = obj.stem.split('_')
-        card_name: str = f'{card_name_first} | {card_name_second}'
-    else:
-        order, card_name = obj.stem.split('_')
-    card_name: str = re_sub(pattern='-', repl=' ', string=card_name)
-    return int(order), card_name
+async def __upload_images_to_telegram(
+    obj: Path,
+    obj_local_path: str,
+    db_obj: dict[str, str | int] | None,
+    dir_name: str,
+) -> dict[str, any]:
+    """Загружает картинки в telegram."""
+    messages_ids: list[int] = []
+    obj_data: dict[str, str] = {}
+
+    for key in ('id_telegram', 'id_telegram_rotated'):
+        while 1:
+            try:
+                if key == 'id_telegram':
+                    message: Message = await bot.send_photo(
+                        chat_id=settings.ADMIN_NOTIFY_ID,
+                        photo=FSInputFile(path=obj),
+                        caption='Perform image res sync...',
+                        disable_notification=True,
+                    )
+                elif obj.is_relative_to(Dirs.DIR_WORDS):
+                    with Image.open(obj) as image:
+                        rotated_img: Image = image.rotate(180, expand=True)
+                        with NamedTemporaryFile(suffix='.jpg') as tmp_file:
+                            rotated_img.save(tmp_file, format='JPEG')
+                            tmp_path: str = tmp_file.name
+                            message: Message = await bot.send_photo(
+                                chat_id=settings.ADMIN_NOTIFY_ID,
+                                photo=FSInputFile(path=tmp_path),
+                                caption='Perform image res sync...',
+                                disable_notification=True,
+                            )
+                else:
+                    break
+            except TelegramRetryAfter:
+                await asyncio_sleep(15)
+                continue
+            messages_ids.append(message.message_id)
+            obj_data[key] = message.photo[-1].file_id
+            break
+
+    await delete_messages_list(
+        chat_id=message.chat.id,
+        messages_ids=messages_ids,
+    )
+
+    if not db_obj:
+        obj_data.update(
+        {
+            'local_path': obj_local_path,
+            'name': __parse_obj_name(obj=obj, dir_name=dir_name),
+            'category': ImageCategory.get_category_by_dir(dir_name=dir_name),
+        },
+        )
+    return obj_data
