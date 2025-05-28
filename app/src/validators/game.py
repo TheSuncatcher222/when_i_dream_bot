02524038fc761c9e40ff9ae1cbbd5446a7d04e7f
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-28 11:41:23 +0300

    Add GameRoles

--- app/src/validators/game.py
+++ app/src/validators/game.py
@@ -2,3 +2,11 @@ class GameParams:
 
     PLAYERS_MAX: int = 10
     PLAYERS_MIN: int = 4
+
+
+class GameRoles:
+
+    BUKA: str = 'бука'
+    FAIRY: str = 'фея'
+    SANDMAN: str = 'песочный человечек'
+    SLEEPER: str = 'спящий'
