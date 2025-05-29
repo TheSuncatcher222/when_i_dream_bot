9a7f9e71c08a1535558f299eb504b3b50aa22394
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-29 14:01:07 +0300

    Add user_achievement_crud, add UserAchievementCrud.update_by_user_id

---
+++ app/src/crud/user_achievement.py
@@ -0,0 +1,57 @@
+from typing import Any
+
+from sqlalchemy.sql import (
+    select,
+    update,
+)
+from sqlalchemy.sql.dml import Update
+from sqlalchemy.sql.selectable import Select
+
+from app.src.database.base_async_crud import BaseAsyncCrud
+from app.src.database.database import AsyncSession
+from app.src.models.user_achievement import UserAchievement
+
+
+class UserAchievementCrud(BaseAsyncCrud):
+    """Класс CRUD запросов к базе данных к таблице UserAchievement."""
+
+    async def update_by_user_id(
+        self,
+        *,
+        user_id: int,
+        obj_data: dict[str, Any],
+        session: AsyncSession,
+        perform_check_unique: bool = False,
+        perform_cleanup: bool = True,
+        perform_commit: bool = True,
+    ) -> UserAchievement:
+        """Обновляет один объект из базы данных по указанному user_id."""
+        if perform_cleanup:
+            obj_data: dict[str, Any] = self._clean_obj_data_non_model_fields(obj_data=obj_data)
+
+        query: Select = select(UserAchievement).where(UserAchievement.user_id == user_id)
+        if (await session.execute(query)).scalars().first() is None:
+            self._raise_value_error_not_found(id=user_id)
+
+        if perform_check_unique:
+            await self._check_unique(obj_data=obj_data, session=session)
+
+        stmt: Update = (
+            update(UserAchievement)
+            .where(UserAchievement.user_id == user_id)
+            .values(**obj_data)
+            .returning(self.model)
+        )
+        obj: UserAchievement = (await session.execute(stmt)).scalars().first()
+
+        if perform_commit:
+            await session.commit()
+
+        return obj
+
+
+user_achievement_crud: UserAchievementCrud = UserAchievementCrud(
+    model=UserAchievement,
+    unique_columns=('user_id',),
+    unique_columns_err='Пользователь с таким user_id уже добавлен в базу данных',
+)
