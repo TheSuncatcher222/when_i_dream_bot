782962cde3c12ec89520405c5d00f63f254b72bf
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-30 18:50:28 +0300

    Add SchedulerJobNames.GAME_END_ROUND, add AsyncIOScheduler.timezone attr

--- app/src/scheduler/scheduler.py
+++ app/src/scheduler/scheduler.py
@@ -2,9 +2,11 @@ from apscheduler.executors.asyncio import AsyncIOExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from app.src.config.config import Timezones
 from app.src.database.database import sync_engine
 
 scheduler: AsyncIOScheduler = AsyncIOScheduler(
+    timezone=Timezones.MOSCOW,
     executors={'default': AsyncIOExecutor()},
     jobstores={'default': SQLAlchemyJobStore(engine=sync_engine)},
 )
@@ -15,4 +17,8 @@ class SchedulerJobNames:
     Класс представления названий (id) задач планировщика.
     """
 
-    SYNC_IMAGES = 'sync_images'
+    # Image.
+    SYNC_IMAGES: str = 'sync_images'
+
+    # Game.
+    GAME_END_ROUND: str = 'game_{number}_end_round'
