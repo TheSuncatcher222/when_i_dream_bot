17c89bc4564c7c1358630f07c65a5e0a24f8e539
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-31 13:22:26 +0300

    Update delete_messages_list logic, refactor

--- app/src/bot/routers/start.py
+++ app/src/bot/routers/start.py
@@ -23,41 +23,21 @@ router: Router = Router()
 async def command_start(message: Message) -> None:
     """
     Обрабатывает команду /start и регистрирует/обновляет пользователя.
+
+    Удаляет старое start-сообщение и присылает новое.
     """
-    answer: Message = await message.answer(
-        text=(
-            'Добро пожаловать, о чудесный сновидец!'
-            '\n\n'
-            'Сегодня ты отправляешься в невероятное путешествие по миру снов. Миру, '
-            'где обитают сказочные создания, вершится магия и процветает фантазия. '
-            'Там ты встретишь друзей и врагов, правдолюбов и лжецов, людей искренних '
-            'и лицемерных.'
-            '\n\n'
-            'Чтобы успешно пройти через все испытания тебе нужно проявить смелость, '
-            'находчивость, смекалку и внутреннюю интуицию. Желаю приятных снов!'
-        ),
-        reply_markup=get_keyboard_main_menu(user_id_telegram=message.from_user.id),
-    )
-    await message.answer_media_group(
-        media=[
-            InputMediaPhoto(media=media_id)
-            for media_id
-            in await get_rules_ids_telegram()
-        ],
-    )
+    messages_ids: list[int] = [message.message_id]
 
     async with async_session_maker() as session:
         user: User | None = await user_crud.retrieve_by_id_telegram(
             obj_id_telegram=message.from_user.id,
             session=session,
         )
-        messages_ids: list[int] = [message.message_id]
 
         if not user:
             user: User = await user_crud.create(
                 obj_data={
                     'id_telegram': str(message.from_user.id),
-                    'message_main_last_id': answer.message_id,
                     'name_first': message.from_user.first_name,
                     'name_last': message.from_user.last_name,
                     'username': message.from_user.username,
@@ -65,12 +45,33 @@ async def command_start(message: Message) -> None:
                 session=session,
                 perform_commit=False,
             )
+            await message.answer_media_group(
+                media=[
+                    InputMediaPhoto(media=media_id)
+                    for media_id
+                    in await get_rules_ids_telegram()
+                ],
+            )
         elif user.message_main_last_id:
-            messages_ids: list[int] = list(range(user.message_main_last_id, message.message_id + 1))
-
+            messages_ids.append(user.message_main_last_id)
         await delete_messages_list(
             chat_id=message.chat.id,
             messages_ids=messages_ids,
+        )
+
+        answer: Message = await message.answer(
+            text=(
+                'Добро пожаловать, о чудесный сновидец!'
+                '\n\n'
+                'Сегодня ты отправляешься в невероятное путешествие по миру снов. Миру, '
+                'где обитают сказочные создания, вершится магия и процветает фантазия. '
+                'Там ты встретишь друзей и врагов, правдолюбов и лжецов, людей искренних '
+                'и лицемерных.'
+                '\n\n'
+                'Чтобы успешно пройти через все испытания тебе нужно проявить смелость, '
+                'находчивость, смекалку и внутреннюю интуицию. Желаю приятных снов!'
+            ),
+            reply_markup=get_keyboard_main_menu(user_id_telegram=message.from_user.id),
         )
         await user_crud.update_by_id(
             obj_id=user.id,
