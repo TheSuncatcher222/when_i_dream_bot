ef8b84cce363174224306fc178d50d5fe76ca895
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-31 13:22:53 +0300

    Register game_join

--- app/src/bot/dispatcher.py
+++ app/src/bot/dispatcher.py
@@ -7,6 +7,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from app.src.bot.routers.fallback import router as fallback
 from app.src.bot.routers.help import router as help
 from app.src.bot.routers.game_create import router  as game_create
+from app.src.bot.routers.game_join import router as game_join
 from app.src.bot.routers.ping import router as ping
 from app.src.bot.routers.start import router as start
 from app.src.bot.routers.sync_images import router as sync_images
@@ -18,6 +19,7 @@ dp: Dispatcher = Dispatcher(
 routers: tuple[Router] = (
     help,
     game_create,
+    game_join,
     ping,
     sync_images,
 )
