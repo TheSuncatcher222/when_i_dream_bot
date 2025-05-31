b4a7f5d5964a8e0688cab47827bfb8b0acbbd77f
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-05-31 13:21:28 +0300

    Add, add in_help

--- app/src/bot/routers/help.py
+++ app/src/bot/routers/help.py
@@ -1,26 +1,51 @@
-
+from typing import Any
 
 from aiogram import (
     Router,
     F,
 )
-from aiogram.types import Message
 
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import (
+    StatesGroup,
+    State,
+)
+from aiogram.types import (
+    InputMediaPhoto,
+    Message,
+)
 
+from app.src.bot.routers.start import command_start
+from app.src.utils.image import get_rules_ids_telegram
+from app.src.utils.message import delete_messages_list
 from app.src.utils.reply_keyboard import (
     RoutersCommands,
-    KEYBOARD_HOME,
+    KEYBOARD_HELP,
 )
 
 router: Router = Router()
 
 
+class HelpForm(StatesGroup):
+    """
+    Состояния формы роутера.
+    """
+
+    in_help = State()
+
+    _help_message_id: int
+
+
 @router.message(F.text == RoutersCommands.HELP)
-async def help(message: Message) -> None:
+async def help(
+    message: Message,
+    state: FSMContext,
+) -> None:
     """
     Обрабатывает команду "Помощь".
     """
-    await message.answer(
+    await delete_messages_list(chat_id=message.chat.id, messages_ids=[message.message_id])
+    answer: Message = await message.answer(
         text=(
             'Предисловие.\n\n'
 
@@ -44,5 +69,40 @@ async def help(message: Message) -> None:
             'нужно указать только номер лобби. Если номер указан верно, бот запросит '
             'пароль для входа - его нужно узнать у того, кто создавал игру (см. пункт 1).'
         ),
-        reply_markup=KEYBOARD_HOME,
+        reply_markup=KEYBOARD_HELP,
     )
+    await state.set_state(HelpForm.in_help)
+    await state.update_data(_help_message_id=answer.message_id)
+
+
+@router.message(HelpForm.in_help)
+async def in_help(
+    message: Message,
+    state: FSMContext,
+) -> None:
+    if message.text == RoutersCommands.HELP_RULES:
+        await delete_messages_list(
+            chat_id=message.chat.id,
+            messages_ids=[message.message_id],
+        )
+        return await message.answer_media_group(
+            media=[
+                InputMediaPhoto(media=media_id)
+                for media_id
+                in await get_rules_ids_telegram()
+            ],
+        )
+
+    if message.text != RoutersCommands.HOME:
+        return await delete_messages_list(
+            chat_id=message.chat.id,
+            messages_ids=[message.message_id],
+        )
+
+    state_data: dict[str, Any] = await state.get_data()
+    await delete_messages_list(
+        chat_id=message.chat.id,
+        messages_ids=[message.message_id, state_data['_help_message_id']],
+    )
+    await state.clear()
+    await command_start(message=message)
