570b0c8dceb30c9b60ce1b275d401769ea75405e
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-06-05 11:07:53 +0300

    Update delete_messages_list in fallback

--- app/src/bot/routers/fallback.py
+++ app/src/bot/routers/fallback.py
@@ -16,14 +16,7 @@ async def fallback(message: Message):
     if message.chat.type != 'private':
         return
 
-    if message.text == RoutersCommands.HOME:
-        await delete_messages_list(
-            chat_id=message.chat.id,
-            messages_ids=list(range(message.message_id - 10, message.message_id + 1)),
-        )
-        return await command_start(message=message)
+    await delete_messages_list(chat_id=message.chat.id, messages_ids=(message.message_id,))
 
-    await delete_messages_list(
-        chat_id=message.chat.id,
-        messages_ids=[message.message_id],
-    )
+    if message.text == RoutersCommands.HOME:
+        return await command_start(message=message)
